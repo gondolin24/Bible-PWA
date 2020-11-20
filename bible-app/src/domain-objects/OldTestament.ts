9a41ba0ed781773b04cfcd6b5847da3cdeae4d3a
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-11-19 21:55:26 -0500

    lots and lots of books

--- bible-app/src/domain-objects/OldTestament.ts
+++ bible-app/src/domain-objects/OldTestament.ts
@@ -5,6 +5,15 @@ import * as GENESIS from '../bible/oldTestament/Genesis.json'
 import * as EXODUS from '../bible/oldTestament/Exodus.json'
 import * as NUMBERS_BOOK from '../bible/oldTestament/Numbers.json'
 import * as LEVEITICUS from '../bible/oldTestament/Leviticus.json'
+import * as DEUTERONOMY from '../bible/oldTestament/Deuteronomy.json'
+import * as JOSHUA from '../bible/oldTestament/Joshua.json'
+import * as JUDGES from '../bible/oldTestament/Judges.json'
+import * as RUTH from '../bible/oldTestament/Ruth.json'
+import * as FIRST_SAMUEL from '../bible/oldTestament/1Samuel.json'
+import * as SECOND_SAMUEL from '../bible/oldTestament/2Samuel.json'
+import * as FIRST_KINGS from '../bible/oldTestament/1Kings.json'
+import * as SECOND_KINGS from '../bible/oldTestament/2Kings.json'
+import * as EZRA from '../bible/oldTestament/Ezra.json'
 
 
 import {BibleBook} from "./BibleBook";
@@ -19,8 +28,39 @@ export class OldTestament extends Testament {
             EXODUS,
             LEVEITICUS,
             NUMBERS_BOOK,
+            DEUTERONOMY,
+            JOSHUA,
+            JUDGES,
+            RUTH,
+            FIRST_SAMUEL,
+            SECOND_SAMUEL,
+            FIRST_KINGS,
+            SECOND_KINGS,
             FIRST_CHRONICLES,
-            SECOND_CHRONICLES
+            SECOND_CHRONICLES,
+            EZRA,
+            JOB,
+            PSALMS,
+            PROVERBS,
+            ECCLESIASTES,
+            SONG_OF_SOLOMON,
+            ISAIAH,
+            JERMEMIAH,
+            lAMENTATIONS,
+            EZEKIEL,
+            DANIEL,
+            HOSEA,
+            JOEL,
+            AMOS,
+            OBADIAH,
+            JOHAH,
+            MICAH,
+            NAHUM,
+            HABAKKUK,
+            ZEPHANIAH,
+            HAGGAI,
+            ZECHARIAH,
+            MALACHI
         ]
 
         const books = rawBooks.map((book) => new BibleBook(book))
