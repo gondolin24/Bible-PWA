b8b867666e1ce411ae913c3b071ee7c80d059776
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-11-29 22:14:31 -0500

    adding remove from array

--- bible-app/src/components/favorites/ListContainer.tsx
+++ bible-app/src/components/favorites/ListContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {IonContent, IonItem, IonLabel, IonList} from "@ionic/react";
 import DI_CONTAINER from "../../d-i-containers/DependencyInjection";
 
@@ -6,36 +6,56 @@ const globalPersist = DI_CONTAINER.container.GlobalPersister
 
 const ListContainer: React.FC = () => {
 
-   let savedItems = globalPersist.globalObject.savedVerses.map((verse: any, index: any) => {
-        return (
-            <IonItem key={index}>
-                <IonLabel>
-                    <h2>{verse.book}</h2>
-                    <h3>Chapter {verse.chapter} Verse {verse.verse}</h3>
-                </IonLabel>
-            </IonItem>
-        )
-    })
+    const [savedVerse, setSavedVerse] = useState(globalPersist.globalObject.savedVerses)
 
+    const [renderedVerses, setRenderedVerse] = useState()
 
-    if(savedItems.length===0){
-        savedItems =  (
-            <IonItem>
-                <IonLabel>
-                    <h3>
-                    No saved verses
-                    </h3>
-                </IonLabel>
-            </IonItem>
+    const randomOnClick = (selectedVerse: any) => {
+        console.log(selectedVerse)
+        console.log(savedVerse)
 
-        )
+        const newVerses = savedVerse.filter((verse: any) => (verse !== selectedVerse))
+        // verse.book !== selectedVerse.book && verse.chapter !== selectedVerse.chapter && verse.verse !== selectedVerse.verse)
+        console.log(newVerses)
+
+        setSavedVerse(newVerses)
     }
+
+    useEffect(() => {
+
+        let savedItems: any = []
+
+        if (savedVerse.length === 0) {
+            savedItems = [(
+                <IonItem key={Math.random()}>
+                    <IonLabel>
+                        <h3>
+                            No saved verses
+                        </h3>
+                    </IonLabel>
+                </IonItem>
+
+            )]
+        } else {
+            savedItems = savedVerse.map((verse: any, index: any) => {
+                return (
+                    <IonItem key={index} onClick={() => randomOnClick(verse)}>
+                        <IonLabel>
+                            <h2>{verse.book}</h2>
+                            <h3>Chapter {verse.chapter} Verse {verse.verse}</h3>
+                        </IonLabel>
+                    </IonItem>
+                )
+            })
+        }
+        setRenderedVerse(savedItems)
+    }, [savedVerse])
 
 
     return (
         <IonContent fullscreen>
             <IonList>
-                {savedItems}
+                {renderedVerses}
             </IonList>
         </IonContent>
     );
